5c73e27a6584920d7297c51910d7fe05c6b3f836
Feiyue Yu <iamyfy@163.com>
2018-01-04 21:30:49 +0800

    yfy

--- make.py
+++ make.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 import jinja2, yaml, shutil, os, markdown
 
+def loadConfig(filename):
+  with open(filename, 'r') as f:
+    return yaml.load(f)
+
+def writeFile(filename, str):
+  with open(filename, 'w') as f:
+    f.write(str)
+
 def main():
   shutil.rmtree('output', True)
   os.mkdir('output')
   shutil.copy('template/bootstrap.min.css', 'output')
   shutil.copy('template/style.css', 'output')
 
-  with open('content/config.yaml', 'r') as f:
-    config = yaml.load(f)
+  config = loadConfig('content/config.yaml')
   env = jinja2.Environment(loader=jinja2.FileSystemLoader('template'))
   md = markdown.Markdown()
 
@@ -19,14 +26,16 @@ def main():
   config['text'] = html
   template = env.get_template('page.html')
   output = template.render(config)
-  with open('output/index.html', 'w') as f:
-    f.write(output)
+  writeFile('output/index.html', output)
 
   # Generate articles
   config['nav'] = None
+  template = env.get_template('article.html')
+  posts = []
   for root, _, files in os.walk('content'):
     for file in files:
-      if file.endswith('.md') and file != 'index.md':
+      name, suf = os.path.splitext(file)
+      if suf == '.md' and name != 'index' and name != 'contents':
         print(file)
         with open(os.path.join(root, file), 'r') as f:
           state = 0
@@ -38,18 +47,23 @@ def main():
               meta += line
             elif state == 2:
               text += line
-          print(meta)
-          print(text)
+        meta = yaml.load(meta)
+        text = md.convert(text)
+        post = dict(meta)
+        post['filename'] = name + '.html'
+        post['text'] = text
+        config['post'] = post
+        output = template.render(config)
+        writeFile('output/' + post['filename'], output)
+        post = config['post']
+        posts.append((post['title'], post['filename'], post['date']))
+        #posts.append(post)
 
-
-        ''' with open(os.path.join(root, file), 'r') as f:
-          meta = yaml.load_all(f)
-          print(meta)
-          for m in meta:
-            print(1)
-        with open(os.path.join(root, file), 'r') as f:    
-          html = md.convert(f.read())
-          print(html) '''
-        
+  # Generate contents
+  config['nav'] = 'contents'
+  config['posts'] = posts
+  template = env.get_template('contents.html')
+  output = template.render(config)
+  writeFile('output/' + 'contents.html', output)
 
 main()
